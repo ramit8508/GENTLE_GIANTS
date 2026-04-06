15eba2c04079aaa9080888b93b6e10d4c92e0871
Moksh <mumukshujain2466@gmail.com>
2026-04-06 12:08:43 +0530

    feat(auth): add generateAccessToken method using JWT

--- project/backend_1/src/models/user.model.js
+++ project/backend_1/src/models/user.model.js
@@ -84,6 +84,19 @@ userSchema.pre("save",async function(){
 userSchema.methods.isPasswordCorrect=async function(password){
   return await bcrypt.compare(password,this.password)
 }
+userSchema.methods.generateAccessToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      name:this.name
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
 
 const User=mongoose.model("User",userSchema)
 module.exports=User

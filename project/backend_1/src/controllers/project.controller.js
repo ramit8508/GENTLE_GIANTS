7d7507e730dac9dbf827cb74fdcbd4060fc9ac7f
Moksh <mumukshujain2466@gmail.com>
2026-04-06 14:20:45 +0530

    feat(project): add getAllProjects and searchProject controllers with filter and populate

--- project/backend_1/src/controllers/project.controller.js
+++ project/backend_1/src/controllers/project.controller.js
@@ -24,5 +24,57 @@ const createProject=async(req,res)=>{
         throw new ApiError(500,"Something went wrong while creating a project",error.message)
     }
 }
-
-module.exports={createProject}
+const getAllProjects=async(req,res)=>{
+    try {
+        const projects=await projectModel.find().select("-createdAt -updatedAt")
+        if(!projects)
+        {
+            throw new ApiError(404,"No projects found")
+        }
+        return res.status(200).json(new ApiResponse(200,"Projects fetched successfully",projects))
+    }
+    catch (error) {
+        throw new ApiError(500,"Something went wrong while fetching projects",error.message)
+    }
+}
+const searchProject=async(req,res)=>{
+    try {
+        const {q}=req.query
+        let query ={}
+        if(q)
+        {
+            const regex=new RegExp(q,"i")
+                   query.$or = [
+                    { title: { $regex: regex } },
+                    { tech_stack: { $regex: regex } },
+                    { roles_needed: { $regex: regex } }
+                ];
+        }
+         const techStack = req.query.tech_stack || req.query['tech_stack[]'];
+        if (techStack) 
+        {
+                const techArray = Array.isArray(techStack) ? techStack : [techStack];
+                query.tech_stack = { $in: techArray };
+        }
+        const roles = req.query.roles || req.query['roles[]'];
+        if (roles) 
+        {
+            const rolesArray = Array.isArray(roles) ? roles : [roles];
+            query.roles_needed = { $in: rolesArray };
+        }
+        if (Object.keys(query).length === 0) 
+        {
+            const projects = await projectModel.find()
+                .populate("createdBy", "name")
+                .populate("members.user", "name");
+            return res.status(200).json(new ApiResponse(200,"Projects fetched successfully",projects));
+        }
+        const projects = await projectModel.find(query)
+            .populate("createdBy", "name")
+            .populate("members.user", "name");
+        return res.status(200).json(new ApiResponse(200,"Projects fetched successfully",projects))
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while fetching projects",error.message)
+    }
+}
+module.exports={createProject,getAllProjects,searchProject}
--- project/backend_1/src/routes/project.route.js
+++ project/backend_1/src/routes/project.route.js
@@ -3,4 +3,6 @@ const projectControllers=require("../controllers/project.controller")
 const {authmiddleware}=require("../middlewares/auth.middleware")
 const router=express.Router()
 router.route("/create").post(authmiddleware,projectControllers.createProject)
+router.route("/").get(projectControllers.getAllProjects)
+router.route("/search").get(projectControllers.searchProject)
 module.exports=router

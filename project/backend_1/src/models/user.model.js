0b058e41a03e630904ca675ee8bdf91e83f3b24f
Moksh <mumukshujain2466@gmail.com>
2026-04-06 12:11:45 +0530

    feat(auth): add generateRefreshToken method using JWT

--- project/backend_1/src/models/user.model.js
+++ project/backend_1/src/models/user.model.js
@@ -97,6 +97,17 @@ userSchema.methods.generateAccessToken=function(){
     }
   )
 }
+userSchema.methods.generateRefreshToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 
 const User=mongoose.model("User",userSchema)
 module.exports=User
